def69f571adb6a0c72910e8d2173d2e21ecd4552
kiketion <kiketionje@gmail.com>
2022-03-29 16:07:11 +0300

    bootstrap running

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import MovieBox from './MovieBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const API_URL =
   'https://api.themoviedb.org/3/movie/popular?api_key=94c5bd7ffcd2c2633d9a7d9054fd8d41';
