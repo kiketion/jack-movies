3c6cdbf25e68ca314eee29407b09f9b681a7a976
Jorge Da Rocha <kiketion@kike-MacBookPro.local>
2022-03-31 15:41:45 +0300

    changes

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Modal, show, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
 
 const API_IMG = 'https://image.tmdb.org/t/p/w500';
 
@@ -9,14 +10,43 @@ const MovieBox = ({
   release_date,
   overview,
 }) => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
-    <div className='card text-center bg-warning bg-opacity-50 mb-3'>
+    <div className='card text-center bg-light mb-3'>
       <div className='card-body'>
         <img className='card-img-top' src={API_IMG + poster_path} alt='' />
+        <h5 className='card-title'>{title}</h5>
         <div className='card-body'>
-          <button type='button' className='btn btn-dark'>
+          <button type='button' className='btn btn-dark' onClick={handleShow}>
             View More
           </button>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+              <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img
+                className='card-img-top'
+                style={{ width: '14rem' }}
+                src={API_IMG + poster_path}
+                alt=''
+              />
+              <h3>{title}</h3>
+              <h4>ImDb: {vote_average}</h4>
+              <h5>Release Date: {release_date}</h5>
+              <br />
+              <h6>Overview</h6>
+              <p>{overview}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant='secondary' onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
     </div>
