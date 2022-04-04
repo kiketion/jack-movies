7e7b853fffbd873c2ccabf33c8b437bcf4bb8a24
Jorge Da Rocha <kiketion@kike-MacBookPro.local>
2022-04-04 12:39:18 +0300

    increasing size pictures

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,4 +1,4 @@
-import { Modal, show, Button } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import React, { useState } from 'react';
 
 const API_IMG = 'https://image.tmdb.org/t/p/w500';
@@ -11,43 +11,42 @@ const MovieBox = ({
   overview,
 }) => {
   const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(!show);
+  };
 
   return (
-    <div className='text-center bg-light mb-3'>
-      <div className='card'>
-        <img
-          className='card-img'
-          src={API_IMG + poster_path}
-          onClick={handleShow}
-          alt=''
-        />
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header>
-            <Modal.Title>{title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <img
-              className='card-img-top'
-              style={{ width: '14rem' }}
-              src={API_IMG + poster_path}
-              alt=''
-            />
-            <h3>{title}</h3>
-            <h4>ImDb: {vote_average}</h4>
-            <h5>Release Date: {release_date}</h5>
-            <br />
-            <h6>Overview</h6>
-            <p>{overview}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+    <div className='card'>
+      <img
+        className='card-img'
+        src={API_IMG + poster_path}
+        onClick={handleShow}
+        alt=''
+      />
+      <Modal show={show} onHide={handleShow}>
+        <Modal.Header>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            className='card-img-top'
+            style={{ width: '14rem' }}
+            src={API_IMG + poster_path}
+            alt=''
+          />
+          <h3>{title}</h3>
+          <h4>ImDb: {vote_average}</h4>
+          <h5>Release Date: {release_date}</h5>
+          <br />
+          <h6>Overview</h6>
+          <p>{overview}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleShow}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
